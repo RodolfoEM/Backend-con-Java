f0152ee50fdeaf70447d09ffd12e4d7ac4204268
LapTaka_G3 <mmcc.20082008@gmail.com>
2023-06-29 23:20:49 -0600

    Castings de datos

---
+++ HolaMundoJava/src/Casting.java
@@ -0,0 +1,20 @@
+public class Casting {
+    public static void main(String[] args) {
+        //En un año 30 perritos
+        //Cuantos perritos al mes
+
+        double monthlyDogs = 30.0/12.0;
+        System.out.println(monthlyDogs);
+
+        //ESTIMACIÓN
+        int estimatedMonthlyDogs = (int) monthlyDogs;
+        System.out.println(estimatedMonthlyDogs);
+
+        //EXACTITUD
+        int a = 30;
+        int b = 12;
+
+        System.out.println(a/b);
+        System.out.println((double) a/b);
+   }
+}
