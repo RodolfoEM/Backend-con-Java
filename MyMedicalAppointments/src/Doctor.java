25555a2786c7a06b9b13fd9cb755e2599dc47171
LapTaka_G3 <mmcc.20082008@gmail.com>
2023-08-01 08:58:32 -0600

    Iniciando Proyecto nuevo de POO con Java

---
+++ MyMedicalAppointments/src/Doctor.java
@@ -0,0 +1,12 @@
+public class Doctor {
+    // Atributos:
+    int id;
+    String name;
+    String speciality;
+
+    // Comportamientos (métodos):
+    public void showName() {
+        // Instrucciones...
+        System.out.println(name);
+    }
+}
