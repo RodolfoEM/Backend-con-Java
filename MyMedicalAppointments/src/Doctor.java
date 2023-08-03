85eb4cf0f24099dbdf77b997fb40d9075edd1cfe
LapTaka_G3 <mmcc.20082008@gmail.com>
2023-08-03 08:53:54 -0600

    Usando la palabra reservada static y final

--- MyMedicalAppointments/src/Doctor.java
+++ MyMedicalAppointments/src/Doctor.java
@@ -1,12 +1,25 @@
 public class Doctor {
     // Atributos:
-    int id;
+    static int id = 0; //Autoincrement
     String name;
     String speciality;
+
+    //Método constructor
+    Doctor(){
+        System.out.println("Construyendo el objeto Doctor");
+        id++;
+    }
+
+    Doctor(String name){
+        System.out.println("El nombre del doctor asignado es: " + name);
+    }
 
     // Comportamientos (métodos):
     public void showName() {
         // Instrucciones...
         System.out.println(name);
+    }
+    public void showID(){
+        System.out.println("ID Doctor: " + id);
     }
 }
