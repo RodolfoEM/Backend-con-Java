a9ebd86dfb9c95af7bd0fd797242c247b0ffa55f
LapTaka_G3 <mmcc.20082008@gmail.com>
2023-08-10 08:51:38 -0600

    Aplicando clases anidadas en la clase Doctor

--- MyMedicalAppointments/src/Doctor.java
+++ MyMedicalAppointments/src/Doctor.java
@@ -1,8 +1,14 @@
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Doctor {
     // Atributos:
     static int id = 0; //Autoincrement
-    String name;
-    String speciality;
+    private String name;
+    private String email;
+    private String speciality;
+
+    //AvaibleAppointment
 
     //Método constructor
     Doctor(){
@@ -23,5 +29,48 @@ public class Doctor {
     }
     public void showID(){
         System.out.println("ID Doctor: " + id);
+    }
+
+    ArrayList<AvailableAppointment> availableAppointments = new ArrayList<>();
+    public void addAvailableAppointment(Date date, String time){
+        availableAppointments.add(new AvailableAppointment(date, time));
+    }
+
+    public ArrayList<AvailableAppointment> getAvailableAppointments(){
+        return availableAppointments;
+    }
+
+    public static class AvailableAppointment{
+        private int id;
+        private Date date;
+        private String time;
+        public AvailableAppointment(Date date, String time){
+            this.date=date;
+            this.time=time;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
     }
 }
