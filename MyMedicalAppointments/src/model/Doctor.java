4720799a11e76ff13961ce300cf1138828fb8460
LapTaka_G3 <mmcc.20082008@gmail.com>
2023-08-17 09:07:14 -0600

    Aplicando interfaces con ISchedulable

--- MyMedicalAppointments/src/model/Doctor.java
+++ MyMedicalAppointments/src/model/Doctor.java
@@ -1,3 +1,5 @@
+package model;
+
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -16,7 +18,7 @@ public class Doctor extends User{
 
     //Método constructor
 
-    Doctor(String name, String email){
+    public Doctor(String name, String email){
         super(name, email);
         System.out.println("El nombre del doctor asignado es: " + name);
         this.speciality = speciality;
